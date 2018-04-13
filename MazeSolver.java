78f78331d9fcb6f5f645f41aa1c204ee644ac38d
Jess <jyuan5@stuy.edu>
2018-04-13 13:20:12 -0400

    new file

--- MazeSolver.java
+++ MazeSolver.java
@@ -3,49 +3,48 @@
 */
 
 public class MazeSolver {
-    
+
+    public final int[] direction = {Maze.EAST, Maze.NORTH, Maze.SOUTH, Maze.WEST};
 
     public boolean solve( Maze mazeToBeSolved) {
-	if ( mazeToBeSolved.explorerIsOnA() == 0)
+
+	//base cases
+	if ( mazeToBeSolved.explorerIsOnA() == 0){
+	    System.out.println ("base case!");
 	    return true;
-	else if ( mazeToBeSolved.explorerIsOnA() == 1)
+	}
+	else if ( mazeToBeSolved.explorerIsOnA() == 1){
+	    System.out.println ("base case!");
 	    return false;
+	}
+
+	//recursive case
 	else {
+
+	    //drop wall and take snapshot
 	    mazeToBeSolved.dropA( 1);
 	    Maze old = takeSnapshot(mazeToBeSolved);
-	    
-	    mazeToBeSolved.go( 1);
-	    System.out.println (mazeToBeSolved);
-	    if ( solve( mazeToBeSolved) == true)
-		return true;
-	    System.out.println (mazeToBeSolved);
-	    mazeToBeSolved = old;
-	    System.out.println (mazeToBeSolved);
-	    
-	    mazeToBeSolved.go( 2);
-	    System.out.println (mazeToBeSolved);
-	    if ( solve( mazeToBeSolved) == true)
-		return true;
-	    System.out.println (mazeToBeSolved);
-	    mazeToBeSolved = old;
-	    System.out.println (mazeToBeSolved);
-	    
-	    mazeToBeSolved.go( 4);
-	    System.out.println (mazeToBeSolved);
-	    if ( solve( mazeToBeSolved) == true)
-		return true;
-	    System.out.println (mazeToBeSolved);
-	    mazeToBeSolved = old;
-	    System.out.println (mazeToBeSolved);
-	    
-	    mazeToBeSolved.go( 8);
-	    System.out.println (mazeToBeSolved);
-	    if ( solve( mazeToBeSolved) == true)
-		return true;
-	    System.out.println (mazeToBeSolved);
-	    
-	    System.out.println (mazeToBeSolved);
-	    
+	    System.out.println ("wall dropped and snapshot taken" + old);
+
+	    //iterate through the directions
+	    for (int i = 0; i < 4; i++){
+		System.out.println ("next direction " + i );
+		
+		//move the explorer in that direction
+		mazeToBeSolved.go(direction[i]);
+		System.out.println ("explorer moved " + mazeToBeSolved);
+		
+		//answer the problem for new mazeToBeSolved
+		if ( solve( mazeToBeSolved) == true){
+		    System.out.println ("explorer found treasure");
+		    return true;}
+	        System.out.println ("explorer did not find treasure");
+		
+		//restore snapshotted maze if answer to problem is false
+		mazeToBeSolved = old;
+		System.out.println ("snapshot restored " + mazeToBeSolved);
+	    }
+	    //none of the directions return true, explorer cannot reach treasure
 	    return false;
 	}
     }
@@ -55,8 +54,5 @@ public class MazeSolver {
         snapshot = new Maze(candidate);
 	return snapshot;
     }
-
-	
-
 
 }
