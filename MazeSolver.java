d9308b6044402247725cc004c3d7ce390750a84e
Jess <jyuan5@stuy.edu>
2018-04-16 02:16:44 -0400

    Add existing files

--- MazeSolver.java
+++ MazeSolver.java
@@ -5,58 +5,68 @@
 public class MazeSolver {
 
     public final int[] direction = {Maze.WEST, Maze.SOUTH, Maze.NORTH, Maze.EAST};
+    public int solutions;
 
-    public boolean solve( Maze mazeToBeSolved) {
+    public int solve( Maze mazeToBeSolved) {
 
-	//base cases
-	if ( mazeToBeSolved.explorerIsOnA() == 0){
-	    System.out.println ("base case!");
-	    return true;
-	}
-	else if ( mazeToBeSolved.explorerIsOnA() == 1){
-	    System.out.println ("base case!");
-	    return false;
-	}
+        //base cases
+        if ( mazeToBeSolved.explorerIsOnA() == 0){
+            System.out.println ("base case!");
+            System.out.println ( "solutions: " + solutions);
+            return 1;
+        }
+        else if ( mazeToBeSolved.explorerIsOnA() == 1){
+            System.out.println ("base case!");
+            System.out.println ( "solutions: " + solutions);
+            return 0;
+        }
 
-	//recursive case
-	else {
+        //recursive case
+        else {
 
-	    //drop wall and take snapshot
-	    mazeToBeSolved.dropA( 1);
+            mazeToBeSolved.dropA( 1);
 
-	    //iterate through the directions
-	    for (int i = 0; i < 4; i++){
-		System.out.println ("next direction " + i );
-		
-		Maze old = takeSnapshot(mazeToBeSolved);
-	       
-		//move the explorer in that direction
-		System.out.println( "old" + old);
-		mazeToBeSolved.go(direction[i]);
-		System.out.println ("explorer moved " + mazeToBeSolved);
-		System.out.println( "old" + old);
-		
-		//answer the problem for new mazeToBeSolved
-		if ( solve( mazeToBeSolved) == true){
-		    System.out.println ("explorer found treasure");
-		    return true;}
-	        System.out.println ("explorer did not find treasure");
-		
-		 //restore snapshotted maze if answer to problem is false
-		System.out.println( mazeToBeSolved);
-		System.out.println( "old" + old);
-		 mazeToBeSolved = old;
-		 System.out.println ("snapshot restored " + mazeToBeSolved);
-	    }
-	    //none of the directions return true, explorer cannot reach treasure
-	    return false;
-	}
+            for (int i = 0; i < 4; i++){
+                System.out.println ("next direction " + i );
+
+                Maze old = takeSnapshot(mazeToBeSolved);
+                System.out.println( "snapshot taken" + old);
+
+                //move the explorer in that direction
+                //System.out.println( "old" + old);
+                mazeToBeSolved.go(direction[i]);
+                System.out.println ("explorer moved " + mazeToBeSolved);
+                //System.out.println( "old" + old);
+
+                //answer the problem for new mazeToBeSolved
+                if ( solve( mazeToBeSolved) > 0){
+                    System.out.println ("explorer found treasure");
+                    //solutions ++;
+                    System.out.println ( "solutions: " + solutions);
+                    return 1;
+                    //break;
+                }
+                else {
+                    System.out.println ("explorer did not find treasure");
+                    System.out.println ( "solutions: " + solutions);
+
+                    //restore snapshotted maze if answer to problem is false
+                    //System.out.println( mazeToBeSolved);
+                    //System.out.println( "old" + old);
+                    mazeToBeSolved = old;
+                    System.out.println ("snapshot restored " + mazeToBeSolved);
+                }
+            }
+            //none of the directions return true, explorer cannot reach treasure
+
+        }
+        return solutions;
     }
 
     private static Maze takeSnapshot( Maze candidate) {
         Maze snapshot;
         snapshot = new Maze(candidate);
-	return snapshot;
+        return snapshot;
     }
 
 }
--- UserOfMazeSolver.java
+++ UserOfMazeSolver.java
@@ -7,25 +7,25 @@ public class UserOfMazeSolver {
     private static Displayer displayer;
 
     public static void main(String[] commandLine)
-	throws java.io.FileNotFoundException {
+        throws java.io.FileNotFoundException {
         System.out.println();
-        
-        //java UserOfMaze mazes/intersection_treasureNorth.txt 1 1
+
+        //java UserOfMazeSolver mazes/intersection_treasureNorth.txt 1 1
 
         Maze maze = new Maze( commandLine[0]
-			      , Integer.parseInt( commandLine[1])
-			      , Integer.parseInt( commandLine[2])
-			      );
+                             , Integer.parseInt( commandLine[1])
+                             , Integer.parseInt( commandLine[2])
+                            );
         System.out.println( maze + System.lineSeparator());
 
-	MazeSolver testMaze = new MazeSolver();
+        MazeSolver testMaze = new MazeSolver();
         System.out.println(testSolver(testMaze, maze));
 
-	
+
     }
 
-    public static boolean testSolver( MazeSolver solver, Maze x) {
-	return solver.solve(x);
+    public static int testSolver( MazeSolver solver, Maze x) {
+        return solver.solve(x);
     }
 }
 
